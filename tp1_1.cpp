18c79664506312526fc6e6c4e1b5f440adac773a
vale97408 <vale97408@gmail.com>
2024-04-02 20:30:58 -0300

    2do commit: Agregado de código en el archivo .cpp (Ej 3e)

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,7 +2,29 @@
 
 int main() 
 {
-  printf("Hola mundo");
+  printf("Hola mundo\n");
+
+  int variable=5;
+  int *puntero;
+  puntero= &variable;
+
+  // Contenido del puntero
+  printf("Contenido del puntero: %d \n", *puntero);
+
+  // Dirección de memoria almacenada en el puntero
+  printf("Dirección de memoria almacenada en el puntero: %p \n", puntero);
+
+ // Dirección de memoria de la variable
+  printf("Dirección de memoria de la variable: %p \n", &variable);
+
+ // Dirección de memoria del puntero 
+  printf("Dirección de memoria del puntero: %p \n", &puntero);
+
+ // el tamaño de memoria utilizado por esa variable usando la función sizeof()
+  printf("El tamaño de memoria utilizado por esa variable usando la función sizeof(): %zu bytes \n", sizeof(variable) );
+
+
+
 
     return 0;
 }
